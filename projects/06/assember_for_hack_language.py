c7231680ce773b0eb09686bbd99e8c22dadb326c
gaopinghuang0 <gaopinghuang@gmail.com>
2017-08-26 18:30:05 -0400

    init project 06 assembler

---
+++ projects/06/assember_for_hack_language.py
@@ -0,0 +1,22 @@
+'''
+Assembler for Hack language
+Translate *.asm file into Hack language *.hack
+Part of project 6 of www.nand2tetris.org
+and the book "The Elements of Computing Systems"
+
+Author: Gaoping Huang
+Since: 20170826
+'''
+
+
+class Assembler(object):
+  def __init__(self, arg):
+    super(Assembler, self).__init__()
+    self.arg = arg
+
+  def translate(self, filename):
+    '''
+    Translate *.asm file into Hack language *.hack
+    '''
+    pass
+    
