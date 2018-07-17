d9db05e865211069905fd608fb68f2662936073a
gaopinghuang0 <gaopinghuang@gmail.com>
2018-07-16 22:45:32 -0400

    working on CompilationEngine

--- projects/10/core/compilation_engine.py
+++ projects/10/core/compilation_engine.py
@@ -52,10 +52,16 @@ class CompilationEngine(object):
         else:
             raise ValueError('should have more tokens')
 
+    def _consume_symbol(self, token=None):
+        self._consume('symbol', token)
+
+    def _consume_keyword(self, token=None):
+        self._consume('keyword', token)
+
     def _compile_type(self):
         token = self.tokenizer.peek_next()
         if token in ['int', 'char', 'boolean']:
-            self._consume('keyword')
+            self._consume_keyword()
         else:
             self._consume('identifier')  # className
 
@@ -69,22 +75,46 @@ class CompilationEngine(object):
         self._consume('identifier')
 
     def _compile_subroutine_body(self):
-        pass
+        self._start_block('subroutineBody')
+        self._consume_symbol('{')
+        while self.compile_var_dec():
+            pass
+        self.compile_statements()
+        # self._consume_symbol('}')  # TODO
+        self._end_block('subroutineBody')
+
+    def _compile_statement(self):
+        token = self.tokenizer.peek_next()
+        if token == 'let':
+            self.compile_let()
+        elif token == 'if':
+            self.compile_if()
+        elif token == 'while':
+            self.compile_while()
+        elif token == 'do':
+            self.compile_do()
+        elif token == 'return':
+            self.compile_return()
+        else:
+            return False
+        return True
+
+
 
     #### Below are public APIs ####
 
     def compile_class(self):
         self._start_block('class')
-        self._consume('keyword', 'class')
+        self._consume_keyword('class')
         self._compile_class_name()
-        self._consume('symbol', '{')
+        self._consume_symbol('{')
         # 0 or more classVarDec
         while self.compile_class_var_dec():
             pass
         # 0 or more subroutineDec
         while self.compile_subroutine():
             pass
-        # self._consume('symbol', '}')  # TODO: uncomment it
+        # self._consume_symbol('}')  # TODO: uncomment it
         self._end_block('class')
 
     def compile_class_var_dec(self):
@@ -93,17 +123,18 @@ class CompilationEngine(object):
             # no classVarDec, just skip
             return False
         self._start_block('classVarDec')
-        self._consume('keyword')  # 'static' or 'field'
+        self._consume_keyword()  # 'static' or 'field'
         self._compile_type()
         self._compile_var_name()  # varName
+        # 0 or more varName
         while True:
             token = self.tokenizer.peek_next()
             if token == ',':
-                self._consume('symbol', ',')
+                self._consume_symbol(',')
                 self._compile_var_name()
             else:
                 break
-        self._consume('symbol', ';')
+        self._consume_symbol(';')
         self._end_block('classVarDec')
         return True
 
@@ -113,36 +144,84 @@ class CompilationEngine(object):
             # no subroutineDec, just skip
             return False
         self._start_block('subroutineDec')
-        self._consume('keyword')  # 'constructor', 'function', 'method'
+        self._consume_keyword()  # 'constructor', 'function', 'method'
         # (void | type)
         token = self.tokenizer.peek_next()
         if token == 'void':
-            self._consume('keyword', 'void')
+            self._consume_keyword('void')
         else:
             self._compile_type()
 
         self._compile_subroutine_name()
-        self._consume('symbol', '(')
+        self._consume_symbol('(')
         self.compile_parameter_list()
-        # self._consume('symbol', ')')
-
+        self._consume_symbol(')')
+        self._compile_subroutine_body()
         self._end_block('subroutineDec')
         return True
 
     def compile_parameter_list(self):
-        pass
+        self._start_block('parameterList')
+        token = self.tokenizer.peek_next()
+        if token == ')':   # no param list, skip
+            self._end_block('parameterList')
+            return
+        self._compile_type()
+        self._compile_var_name()
+        # 0 or more (type, varName)
+        while True:
+            token = self.tokenizer.peek_next()
+            if token == ',':
+                self._consume_symbol(',')
+                self._compile_type()
+                self._compile_var_name()
+            else:
+                break
+        self._end_block('parameterList')
 
     def compile_var_dec(self):
-        pass
+        token = self.tokenizer.peek_next()
+        if token != 'var':   # no var dec, skip
+            return False
+        self._start_block('varDec')
+        self._consume_keyword('var')
+        self._compile_type()
+        self._compile_var_name()
+        # 0 or more varName
+        while True:
+            token = self.tokenizer.peek_next()
+            if token == ',':
+                self._consume_symbol(',')
+                self._compile_var_name()
+            else:
+                break
+        self._consume_symbol(';')
+        self._end_block('varDec')
+        return True
 
     def compile_statements(self):
-        pass
+        self._start_block('statements')
+        while self._compile_statement():
+            pass
+        self._end_block('statements')
 
     def compile_do(self):
         pass
 
     def compile_let(self):
-        pass
+        self._start_block('letStatement')
+        self._consume_keyword('let')
+        self._compile_var_name()
+        # ('[' expression ']')?
+        token = self.tokenizer.peek_next()
+        if token == '[':  # array left bracket
+            self._consume_symbol('[')
+            self.compile_expression()
+            self._consume_symbol(']')
+        self._consume_symbol('=')
+        self.compile_expression()
+        # self._consume_symbol(';')  # TODO
+        self._end_block('letStatement')
 
     def compile_while(self):
         pass
